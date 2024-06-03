f358eb4193f5dcd3490dc35872384ef569b94787
AmrithNiyogi <70276074+AmrithNiyogi@users.noreply.github.com>
2024-06-03 12:29:48 +0530

    Update check_number_type.py

--- Conditionals/check_number_type.py
+++ Conditionals/check_number_type.py
@@ -1,13 +1,13 @@
-def check_number(num):
+def check_number_type(num):
     if num == 0:
         print("Number is 0")
-    elif num % 2 == 0:
-        print(f"{num} is even")
+    elif num < 0:
+        print(f"{num} is negative")
     else:
-        print(f"{num} is odd")
+        print(f"{num} is positive")
 
 
 if __name__ == "__main__":
     number = int(input("Enter a number: "))
-    check_number(number)
+    check_number_type(number)
     
