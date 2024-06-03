c7422ea028ebc735e55656a0c5969553b0eb01b3
AmrithNiyogi <70276074+AmrithNiyogi@users.noreply.github.com>
2024-06-03 18:27:20 +0530

    Delete Basic Operation/square_of_number.py

--- Basic Operation/square_of_number.py
+++
@@ -1,8 +0,0 @@
-def square_number(num):
-    return num ** 2
-
-
-if __name__ == '__main__':
-    number1 = int(input("Enter a number:"))
-    result = square_number(number1)
-    print(f"Square of {number1} : {result}")
