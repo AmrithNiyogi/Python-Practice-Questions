065ca9d0122acad305d259934c65fa5958687777
AmrithNiyogi <70276074+AmrithNiyogi@users.noreply.github.com>
2024-06-05 21:56:00 +0530

    Create interate_list.py

---
+++ Lists/Iterate Through Lists/interate_list.py
@@ -0,0 +1,13 @@
+if __name__ == '__main__':
+    countries = ["USA", "UK", "France", "Germany"]
+    print("Printing the list of countries using a for loop")
+    for country in countries:
+        print(country)
+
+    index = 0
+    fruits = ["apple", "banana", "cherry"]
+    print("Printing the list of fruits using a while loop")
+    while index < len(fruits):
+        print(fruits[index])
+        index += 1
+        
