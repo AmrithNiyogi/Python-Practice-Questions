a4ad47a275f2b69ad6826d45fb0d45613bc54a1c
AmrithNiyogi <70276074+AmrithNiyogi@users.noreply.github.com>
2024-06-06 18:31:26 +0530

    Create combine_split_list.py

---
+++ Lists/Combining and Splitting a List/combine_split_list.py
@@ -0,0 +1,12 @@
+if __name__ == '__main__':
+    list1 = [1, 2, 3]
+    print(f"List 1: {list1}")
+    list2 = [4, 5, 6]
+    print(f"List 2: {list2}")
+    combined_list = list1 + list2
+    print(f"Combined list: {combined_list}\n")
+
+    sentence = "This is a test sentence"
+    print(f"Sentence: {sentence}")
+    words = sentence.split()
+    print(f"Sentence after splitting: {words}")
